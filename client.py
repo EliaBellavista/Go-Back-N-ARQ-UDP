376aac00427d3dfbeece77bb7ac55713b31555c8
Elia Bellavista <elia.bellavista@studio.unibo.it>
2025-06-06 17:23:03 +0200

    fixed

--- client.py
+++ client.py
@@ -1,27 +1,97 @@
 import time, socket, sys
+import threading
 
+running = True
 address = ('localhost', 12345)
-window = 16
-received = [] 
-next = 0
+window = 4
+max_window = 8
+timeout = 2
+lock = threading.Lock()
+sent_window = {} 
+SeqN = 0
+delay = 0.2
+index = 0
+confirmed_packets = 0
+total_acks = 0
+total_packets = 0
+
+def is_newer(seq1, seq2): #treu if seq1 is newer than seq2
+    return (0 < (seq1 - seq2) % max_window <= max_window // 2)
+
+def elaborate_ACKS(socket):
+    print("hello from ack thread")
+    global sent_window, confirmed_packets, total_acks, running
+    while running:
+        try:
+            ack, address = socket.recvfrom(4096)
+            total_acks += 1
+            ack = ack.decode('utf8')
+            ack = int(ack)
+            print("received ack: %s" % ack)
+            with lock:
+                keys = list(sent_window.keys())
+                for i in keys:
+                    if is_newer(ack, i):
+                        confirmed_packets += 1
+                        del sent_window[i]
+                
+        except Exception as e:
+            print("Error receiving ACK: ", e)
+            continue
+
+message = input("Inserisci stringa da inviare: ")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto("hello!".encode(), address)
 
-print("hello")
-sock.sendto(str(window).encode(), address)
+#thread per ricevere ack mentre invio pacchetti
+thread_ack = threading.Thread(target=elaborate_ACKS, args=(sock,))
+thread_ack.daemon = True
+thread_ack.start()
+print("started ack receiving thread")
 
-while True:
-    time.sleep(0.4)  # rallento il client a scopo didaatico
-    data, address = sock.recvfrom(4096)
-    data = data.decode('utf8')
-    if int(data) == next:
-        print("received : %s, next: %d" % (data, next))
-        received.append(data)
-        next = (next + 1) % window
-    else:
-        print("received out of order packet: %s, expected: %d" % (data, next))
-    
-    #invio ack
-    sock.sendto(str(next).encode(), address)
+while running:
+    print("inizio invio finestra")
+    while len(sent_window) < window: #continua finchè c`è spazio nella finestra
+        time.sleep(delay)
 
-#TODO timeout
+        seq = SeqN.to_bytes(1, 'big')
+        
+        index = confirmed_packets + len(sent_window)
+        if index < len(message):
+            char = message[index].encode('utf-8')
+            print("sending char: %s" % char.decode('utf-8'))
+        else:
+            char = b'\xFF'
+
+        packet = seq + char 
+
+        print("invio pacchetto : %d" % SeqN)
+        sock.sendto(packet, address)
+        total_packets += 1
+        with lock:
+            sent_window[SeqN] = time.time()
+        SeqN = (SeqN + 1) % max_window
+
+    print("finestra completa, check timeout")
+    print("finestra inviata: %s" % str(sent_window.keys()))
+    #check timeout
+    with lock:
+        now = time.time()
+        for seq in list(sent_window.keys()):
+            if now - sent_window[seq] > timeout:
+                print("timeout per il pacchetto con seq: %d" % seq)
+                sent_window = {} #resetto tutta la finestra
+                SeqN = seq
+                break
+
+    if confirmed_packets >= len(message):
+            print("tutti i pacchetti confermati, invio terminato")
+            running = False
+
+    time.sleep(0.2)
+
+sock.close
+print("\ntotal packets sent: %d" % total_packets)
+print("total acks received: %d" % total_acks)
+print("of which %d were correct receptions" % confirmed_packets)
--- server.py
+++ server.py
@@ -1,95 +1,51 @@
-#TODO
-#il server deve: inviare fino a riempimento finestra
-#aspettare gli ack per andare avanti o ripartire dala perdita
-#se riparto e avevo già settato timer o acks, devo azzerarli
-
-#TODO
-# magari un check di ip + porta
-#magari un thread per ogni client?
-
+import random
 import time, socket, sys
-import threading
 
+#TODO
+# magari un check di ip + porta?
+
+address = ('localhost', 12345)
 window = 8
-max_window = 16
-timeout = 1
-lock = threading.Lock()
-sent_window = {} 
-SeqN = 0
-delay = 0.5
+received = [] 
+next = 0
 
-
-def is_newer(seq1, seq2): #treu if seq1 is newer than seq2
-    return (0 < (seq1 - seq2) % max_window <= max_window // 2)
-
-def elaborate_ACKS(socket):
-    print("hello from ack thread")
-    global delay
-    while True:
-        try:
-            time.sleep(0.3)  # sleep to avoid busy waiting
-            ack, address = socket.recvfrom(4096)
-            ack = ack.decode('utf8')
-            ack = int(ack)
-            print("received ack: %s" % ack)
-            with lock:
-                if ack in sent_window:
-                    keys = list(sent_window.keys())
-                    for i in keys:
-                        if is_newer(ack, i):
-                            del sent_window[i]
-                    delay = max(0.01, delay * 0.9)
-                else:
-                    delay = min(1, delay * 1.5)
-                
-        except Exception as e:
-            print("Error receiving ACK: ", e)
-            continue
-
-print("server started")
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('localhost', 12345))
-print("socket binded")
-
-#thread per ricevere ack mentre invio pacchetti
-print("starting ack receiving thread")
-thread_ack = threading.Thread(target=elaborate_ACKS, args=(sock,))
-thread_ack.daemon = True
-thread_ack.start()
-print("started ack receiving thread")
 
 print("in attesa di client")
 data, address = sock.recvfrom(4096)
-#window = data.decode('utf8')
-
 
 while True:
-    print("inizio invio finestra")
-    while len(sent_window) < window: #continua finchè c`è spazio nella finestra
-        print("delay: %f" % delay)
-        time.sleep(delay)
-        print("invio pacchetto : %d" % SeqN)
-        sock.sendto(str(SeqN).encode(), address)
-        with lock:
-            sent_window[SeqN] = time.time()
-        SeqN = (SeqN + 1) % max_window
-    print("finestra comleta, check timeout")
-    print("finestra inviata: %s" % str(sent_window))
-    #check timeout
-    with lock:
-        now = time.time()
-        for seq in list(sent_window.keys()):
-            if now - sent_window[seq] > timeout:
-                print("timeout per il pacchetto con seq: %d" % seq)
-                sent_window = {} #resetto tutta la finestra
-                SeqN = seq
-                break
-    time.sleep(0.4)
+    time.sleep(0.3)  # tempo di elaborazione
+    # con il timer inseruto nel client, aumentare ulteriormente il delay inizia a creare problemi
+    # come perdita di pacchetti e ricezioni out of order
+    try:
+        data, address = sock.recvfrom(4096)
+    except Exception as e:
+        print("Errore durante la ricezione dei dati: ", e)
+        continue
+    seqN = data[0]
+    payload = data[1:]
 
+    if not(random.randint(0, 7)):  # 1/8 di probabilità di perdere il pacchetto
+        print("simulazione pacchetto perso, seqN: %s" % data)
+        continue
 
-'''
-go back N
-ack a ogni ricezione, contenente il numero di sequenza che ci si aspetta di ricevere.
-la ritrasmissione parte al timeout, non al ricevimento dell'ack
-al ricevimento di un ack che fa scorrere la finestra, invio il pacchetto più nuovo
-'''
+    if seqN == next:
+        if payload == b'\xff':
+            break
+
+        payload = payload.decode('utf8')
+        print("received : %s, next: %d" % (seqN, next))
+        received.append(payload)
+        next = (next + 1) % window
+    else:
+        print("received out of order packet: %s, expected: %d" % (seqN, next))
+
+    sock.sendto(str(next).encode(), address) #ack 
+    
+print(''.join(received))
+print("received all packets, closing server")
+sock.sendto(str(next).encode(), address)
+sock.close()
+sys.exit(0)
